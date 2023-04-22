6b5b7ddb8bb0e8bd3ef32b7e1f6ab9cc73913d7a
Ujjawal <ujjawal@akudo.in>
2023-04-22 11:34:42 +0530

    added build

--- build/types/FaceDetectionOptions.d.ts
+++ build/types/FaceDetectionOptions.d.ts
@@ -3,16 +3,16 @@ import { FaceDetection, Options, Results } from '@mediapipe/face_detection';
 export declare type FaceDetectionResults = Results;
 export declare type FaceDetectionOptions = Options;
 export declare type CameraOptions = {
-    mediaSrc: HTMLVideoElement;
-    onFrame: () => Promise<void>;
-    width: number;
-    height: number;
+  mediaSrc: HTMLVideoElement;
+  onFrame: () => Promise<void>;
+  width: number;
+  height: number;
 };
 export declare type ICamera = (cameraOptions: CameraOptions) => Camera;
 export interface IFaceDetectionOptions {
-    mirrored?: boolean;
-    handleOnResults?: (results: FaceDetectionResults) => void;
-    faceDetectionOptions?: FaceDetectionOptions;
-    faceDetection: FaceDetection;
-    camera?: ICamera;
+  mirrored?: boolean;
+  handleOnResults?: (results: FaceDetectionResults) => void;
+  faceDetectionOptions?: FaceDetectionOptions;
+  faceDetection: FaceDetection;
+  camera?: ICamera;
 }
--- build/types/FaceDetectionReturnType.d.ts
+++ build/types/FaceDetectionReturnType.d.ts
@@ -3,10 +3,10 @@ import { LegacyRef, RefObject } from 'react';
 import Webcam from 'react-webcam';
 export declare type BoundingBox = NormalizedRect;
 export interface IFaceDetectionReturnType {
-    boundingBox: BoundingBox[];
-    isLoading: boolean;
-    detected: boolean;
-    facesDetected: number;
-    webcamRef: LegacyRef<Webcam>;
-    imgRef: RefObject<HTMLImageElement>;
+  boundingBox: BoundingBox[];
+  isLoading: boolean;
+  detected: boolean;
+  facesDetected: number;
+  webcamRef: LegacyRef<Webcam>;
+  imgRef: RefObject<HTMLImageElement>;
 }
